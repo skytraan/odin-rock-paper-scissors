77117caba42eca07bd13f1963e556976d08e5db2
Sky Tran <skytraan@gmail.com>
2026-01-10 18:16:57 -0800

    Created ui for rock paper scissors game, interactable buttons with DOM modification, score resetting after game win

--- game.js
+++ game.js
@@ -1,5 +1,36 @@
+let humanScore = 0;
+let computerScore = 0;
+let playRoundToggle = false;
+
+const rockButton = document.querySelector("#rock-button");
+const paperButton = document.querySelector("#paper-button");
+const scissorsButton = document.querySelector("#scissors-button");
+
+rockButton.addEventListener("click", () => {
+    playRound("rock", getComputerChoice());
+});
+paperButton.addEventListener("click", () => {
+    playRound("paper", getComputerChoice());
+});
+scissorsButton.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice());
+});
+
+const resultsContainer = document.querySelector("#results");
+
+const humanChoiceContainer = document.querySelector("#human-choice");
+const computerChoiceContainer = document.querySelector("#computer-choice");
+const winLossContainer = document.querySelector("#win-loss");
+
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random() * 3);
+
+    if (playRoundToggle == false){
+        const scoreContainer = document.querySelector("#score");
+        scoreContainer.textContent = "";
+    }
+    playRoundToggle = true;
+
     switch (computerChoice){
         case 0:
             return "rock";
@@ -13,80 +44,131 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let humanChoice = prompt("Enter your choice: rock | paper | scissors");
-    return humanChoice;
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+
+    switch(humanChoice){
+        case "rock":
+            switch (computerChoice){
+                case "rock": 
+                    humanChoiceContainer.textContent = "You chose: rock."
+                    computerChoiceContainer.textContent = "Computer chose: rock."
+                    winLossContainer.textContent = "Tie! The score is: \n" 
+                                                    + "You: " + humanScore + " | "  + "Computer: " + computerScore;
+                    break;
+                case "scissors":
+                    humanChoiceContainer.textContent = "You chose: rock."
+                    computerChoiceContainer.textContent = "Computer chose: scissors."
+                    humanScore++;
+                    if (humanScore == 5){
+                        winLossContainer.textContent = "Game over! You won!: \n" 
+                                                    + "You: " + humanScore + " | "  + "Computer: " + computerScore; 
+                        humanScore = 0;
+                        computerScore = 0;
+                    }
+                    else {
+                        winLossContainer.textContent = "You win! The score is: \n" 
+                                                    + "You: " + humanScore + " | "  + "Computer: " + computerScore; 
+                    }
+                    break;
+                case "paper":            
+                    humanChoiceContainer.textContent = "You chose: rock."
+                    computerChoiceContainer.textContent = "Computer chose: paper."
+                    computerScore++;
+                    if (computerScore == 5){
+                        winLossContainer.textContent = "Game over! You lost!: \n" 
+                                                    + "You: " + humanScore + " | "  + "Computer: " + computerScore; 
+                        humanScore = 0;
+                        computerScore = 0;
+                    }
+                    else { 
+                        winLossContainer.textContent = "You lose! The score is: \n" 
+                                                    + "You: " + humanScore + " | "  + "Computer: " + computerScore;
+                    }
+                    break;
+            }
+            break;
+        case "scissors":
+            switch (computerChoice){
+                case "rock":
+                    humanChoiceContainer.textContent = "You chose: scissors."
+                    computerChoiceContainer.textContent = "Computer chose: rock."
+                    computerScore++;
+                    if (computerScore == 5){
+                        winLossContainer.textContent = "Game over! You lost!: \n" 
+                                                    + "You: " + humanScore + " | "  + "Computer: " + computerScore; 
+                        humanScore = 0;
+                        computerScore = 0;
+                    }
+                    else {
+                        winLossContainer.textContent = "You lose! The score is: \n" 
+                                                    + "You: " + humanScore + " | "  + "Computer: " + computerScore;
+                    }                                                    
+                    break;
+                case "scissors":
+                    humanChoiceContainer.textContent = "You chose: scissors."
+                    computerChoiceContainer.textContent = "Computer chose: scissors."
+                    winLossContainer.textContent = "Tie! The score is: \n" 
+                                                    + "You: " + humanScore + " | "  + "Computer: " + computerScore;
+                    break;
+                case "paper":    
+                    humanChoiceContainer.textContent = "You chose: scissors."
+                    computerChoiceContainer.textContent = "Computer chose: paper."
+                    humanScore++;      
+                    if (humanScore == 5){
+                        winLossContainer.textContent = "Game over! You won!: \n" 
+                                                    + "You: " + humanScore + " | "  + "Computer: " + computerScore; 
+                        humanScore = 0;
+                        computerScore = 0;
+                    }
+                    else {
+                        winLossContainer.textContent = "You win! The score is: \n" 
+                                                    + "You: " + humanScore + " | "  + "Computer: " + computerScore;
+                    }                                                    
+                    break;  
+            }
+            break;
+        case "paper":
+            switch (computerChoice){
+                case "rock":
+                    humanChoiceContainer.textContent = "You chose: paper."
+                    computerChoiceContainer.textContent = "Computer chose: rock."
+                    humanScore++;   
+                    if (humanScore == 5){
+                        winLossContainer.textContent = "Game over! You won!: \n" 
+                                                    + "You: " + humanScore + " | "  + "Computer: " + computerScore; 
+                        humanScore = 0;
+                        computerScore = 0;
+                    }
+                    else {
+                        winLossContainer.textContent = "You win! The score is: \n" 
+                                                    + "You: " + humanScore + " | "  + "Computer: " + computerScore;
+                    }
+                    break;
+                case "scissors":
+                    humanChoiceContainer.textContent = "You chose: paper."
+                    computerChoiceContainer.textContent = "Computer chose: scissors."
+                    computerScore++;
+                    if (computerScore == 5){
+                        winLossContainer.textContent = "Game over! You lost!: \n" 
+                                                    + "You: " + humanScore + " | "  + "Computer: " + computerScore; 
+                        humanScore = 0;
+                        computerScore = 0;
+                    }
+                    else {
+                    winLossContainer.textContent = "You lose! The score is: \n" 
+                                                    + "You: " + humanScore + " | "  + "Computer: " + computerScore;
+                    }
+                    break;
+                case "paper":      
+                    humanChoiceContainer.textContent = "You chose: paper."
+                    computerChoiceContainer.textContent = "Computer chose: paper."
+                    winLossContainer.textContent = "Tie! The score is: \n" 
+                                                    + "You: " + humanScore + " | "  + "Computer: " + computerScore;
+                    return;      
+            }
+            break;
+    }
 }
 
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice){
-        humanChoice = humanChoice.toLowerCase();
-
-        switch(humanChoice){
-            case "rock":
-                switch (computerChoice){
-                    case "rock": 
-                        console.log("Tie! You both chose rock.")
-                        break;
-                    case "scissors":
-                        console.log("You win! Rock beats scissors.")
-                        humanScore++;
-                        break;
-                    case "paper":            
-                        console.log("You lose! Paper beats rock.")
-                        computerScore++;
-                        break;
-                }
-                break;
-            case "scissors":
-                switch (computerChoice){
-                    case "rock":
-                        console.log("You lose! Rock beats scissors.")
-                        computerScore++;
-                        break;
-                    case "scissors":
-                        console.log("Tie! You both chose scissors.")
-                        break;
-                    case "paper":    
-                        console.log("You win! Scissors beats paper.")
-                        humanScore++;      
-                        break;  
-                }
-                break;
-            case "paper":
-                switch (computerChoice){
-                    case "rock":
-                        console.log("You win! Paper beats rock.")
-                        humanScore++;   
-                        break;
-                    case "scissors":
-                        console.log("You lose! Scissors beats paper.")
-                        computerScore++;
-                        break;
-                    case "paper":      
-                        console.log("Tie! You both chose paper.")
-                        return;      
-                }
-                break;
-        }
-    }
-
-    for (let i = 0; i < 5; i++){
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-        console.log("Human score is: " + humanScore);
-        console.log("Computer score is " + computerScore);
-    }
-
-        console.log("Game over!")
-        console.log("Human score is: " + humanScore);
-        console.log("Computer score is " + computerScore);
-
-}
-
-playGame();
